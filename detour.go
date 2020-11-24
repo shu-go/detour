781dd1bc81464f1f7caedc3c6ae1a52ab5fcf4c0
ShuheiKubota <kubota.shuhei+github@gmail.com>
2020-11-24 09:19:36 +0900

    refactoring: crlf->lf

--- detour.go
+++ detour.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"bufio"
-    "fmt"
+	"fmt"
+
 	//"path/filepath"
 	"os"
 	"regexp"
 	"strings"
 	"time"
 
-    "github.com/mattn/go-zglob"
+	"github.com/mattn/go-zglob"
 
 	"github.com/shu-go/gli"
 	"github.com/shu-go/shortcut"
@@ -19,8 +20,8 @@ type globalCmd struct {
 	Rules   Rules  `cli:"rule,r"`
 	RuleSet string `cli:"rule-set=FILENAME"`
 
-    Verbose bool `cli:"verbose,v"`
-    DryRun bool `cli:"dry-run,n"`
+	Verbose bool `cli:"verbose,v"`
+	DryRun  bool `cli:"dry-run,n"`
 }
 
 func (c globalCmd) Run(args []string) error {
@@ -34,56 +35,55 @@ func (c globalCmd) Run(args []string) error {
 		scanner := bufio.NewScanner(f)
 		for scanner.Scan() {
 			line := scanner.Text()
-            if err := c.Rules.Parse(line); err != nil {
-                return nil
-            }
+			if err := c.Rules.Parse(line); err != nil {
+				return nil
+			}
 		}
 	}
 
-    if c.DryRun {
-        fmt.Println("")
-        fmt.Println("DRY RUN MODE")
-        fmt.Println("")
-    }
+	if c.DryRun {
+		fmt.Println("")
+		fmt.Println("DRY RUN MODE")
+		fmt.Println("")
+	}
 
-    if c.Verbose {
-        fmt.Println("Rules:")
-        for _, r := range c.Rules {
-            fmt.Println("  "+r.Old+" => "+r.New)
-        }
-    }
+	if c.Verbose {
+		fmt.Println("Rules:")
+		for _, r := range c.Rules {
+			fmt.Println("  " + r.Old + " => " + r.New)
+		}
+	}
 
-    var files []string
-    if len(args) == 0{
-        args = append(args, "*.lnk")
-    }
-    for _, arg := range args {
-        if !strings.HasSuffix(arg, ".lnk") {
-            arg += ".lnk"
-        }
-        //ff, err := filepath.Glob(arg)
-        ff, err := zglob.Glob(arg)
-        if err != nil {
-            return err
-        }
+	var files []string
+	if len(args) == 0 {
+		args = append(args, "*.lnk")
+	}
+	for _, arg := range args {
+		if !strings.HasSuffix(arg, ".lnk") {
+			arg += ".lnk"
+		}
+		//ff, err := filepath.Glob(arg)
+		ff, err := zglob.Glob(arg)
+		if err != nil {
+			return err
+		}
 
-        
-        files = append(files, ff...)
-    }
+		files = append(files, ff...)
+	}
 
-    if c.Verbose{
-        fmt.Println("Files:")
-    }
+	if c.Verbose {
+		fmt.Println("Files:")
+	}
 	for _, f := range files {
-        fi, err := os.Lstat(f)
-        if err != nil {
-            return err
-        }
-        if fi.IsDir() {
-            continue
-        }
+		fi, err := os.Lstat(f)
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() {
+			continue
+		}
 
-        changed := false
+		changed := false
 
 		s, err := shortcut.Open(f)
 		if err != nil {
@@ -93,29 +93,29 @@ func (c globalCmd) Run(args []string) error {
 		for _, r := range c.Rules {
 			re := regexp.MustCompile("(?i)" + r.Old)
 
-            after := re.ReplaceAllString(s.TargetPath, r.New)
-            if s.TargetPath != after {
-                changed = true
-                s.TargetPath = after
-            }
+			after := re.ReplaceAllString(s.TargetPath, r.New)
+			if s.TargetPath != after {
+				changed = true
+				s.TargetPath = after
+			}
 			after = re.ReplaceAllString(s.WorkingDirectory, r.New)
-            if s.WorkingDirectory != after {
-                changed = true
-                s.WorkingDirectory = after
-            }
+			if s.WorkingDirectory != after {
+				changed = true
+				s.WorkingDirectory = after
+			}
 		}
 
-        if c.Verbose{
-            if changed {
-                fmt.Println("* "+f)
-            } else {
-                fmt.Println("  "+f)
-            }
-        }
+		if c.Verbose {
+			if changed {
+				fmt.Println("* " + f)
+			} else {
+				fmt.Println("  " + f)
+			}
+		}
 
-        if changed && !c.DryRun {
-            s.Save(f)
-        }
+		if changed && !c.DryRun {
+			s.Save(f)
+		}
 	}
 
 	return nil
@@ -135,7 +135,7 @@ func main() {
 	app.Name = "detour"
 	app.Desc = "Windows shortcut replacer tool"
 	app.Version = Version
-    app.Usage = `detour -r old1:new1 -r old2:new2
+	app.Usage = `detour -r old1:new1 -r old2:new2
 detour -r old1:new1 -r old2:new2  ./subdir/*
 detour --rule-set your_rules.txt`
 	app.Copyright = "(C) 2020 Shuhei Kubota"
--- rules.go
+++ rules.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"strings"
 
-    "github.com/dlclark/regexp2"
+	"github.com/dlclark/regexp2"
 )
 
 const Separator = `:`
@@ -14,32 +14,32 @@ type Rule struct {
 }
 
 func (r *Rule) Scan(s string) error {
-    re := regexp2.MustCompile(`(?<!\\)`+Separator, 0)
-    m, err := re.FindStringMatch(s)
-    if err != nil {
-        return err
-    }
+	re := regexp2.MustCompile(`(?<!\\)`+Separator, 0)
+	m, err := re.FindStringMatch(s)
+	if err != nil {
+		return err
+	}
 
 	if m == nil {
-		return errors.New(s +" ... `OLD" + Separator + "NEW` required")
-    }
+		return errors.New(s + " ... `OLD" + Separator + "NEW` required")
+	}
 
-    pos := m.Capture.Index
+	pos := m.Capture.Index
 	if pos == 0 {
 		return errors.New("OLD must not be empty")
 	}
 
-    re2 := regexp2.MustCompile(`(?<!\\)\\`+Separator, 0)
-    t, err := re2.Replace(s[0:pos], Separator, 0, -1)
-    if err != nil {
-        return err
-    }
+	re2 := regexp2.MustCompile(`(?<!\\)\\`+Separator, 0)
+	t, err := re2.Replace(s[0:pos], Separator, 0, -1)
+	if err != nil {
+		return err
+	}
 	r.Old = t
 
-    t, err = re2.Replace(s[pos+len(Separator):], Separator, 0, -1)
-    if err != nil {
-        return err
-    }
+	t, err = re2.Replace(s[pos+len(Separator):], Separator, 0, -1)
+	if err != nil {
+		return err
+	}
 	r.New = t
 
 	return nil
@@ -48,10 +48,10 @@ func (r *Rule) Scan(s string) error {
 type Rules []Rule
 
 func (rr *Rules) Parse(s string) error {
-    // comment
-    if strings.HasPrefix(s, "#") {
-        return nil
-    }
+	// comment
+	if strings.HasPrefix(s, "#") {
+		return nil
+	}
 
 	var r Rule
 	if err := r.Scan(s); err != nil {
